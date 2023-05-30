cb1eab9125b56f2aefb66347b885fb20da99d82c
qwertyNippon <orlandohenry100@gmail.com>
2023-05-29 22:18:58 -0400

    qwerty

--- parkingLotProject.py
+++ parkingLotProject.py
@@ -17,7 +17,7 @@ class Parking():
 # - This should decrease the amount of tickets available by 1
 # - This should decrease the amount of parkingSpaces available by 1
     def pay_parking(self):
-        payment = input(f"Welcome to sustainable pavement car parking.\nIt is $1.00 for 15 mins or parking, select space to continue.\nIf you wish to end press 'q'. ")
+        payment = input(f"Welcome to sustainable pavement car parking.\nIt is $1.00 for 15 mins or parking, entre 1 dollar for 15 minutes.\nIf you wish to end press 'q'. ")
         
         if payment == 'q':
             return
@@ -39,9 +39,13 @@ class Parking():
 # - If the payment variable is not empty then (meaning the ticket has been paid) -> display
 #   a message to the user that their ticket has been paid and they have 15mins to leave
 # - This should update the "currentTicket" dictionary key "paid" to True
-    def leave_garage(self):
+    # def leave_garage(self):
 # - If the ticket has been paid, display a message of "Thank You, have a nice day"
 # - If the ticket has not been paid, display an input prompt for payment
 # - Once paid, display message "Thank you, have a nice day!"
 # - Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
 # - Update tickets list to increase by 1 (meaning add to the tickets list)
+
+qwerty = Parking()
+qwerty.take_ticket()
+qwerty.pay_parking()
