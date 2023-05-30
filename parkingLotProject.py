b3eda5edf2887339cb91cb3d4d89d86a8c168a0d
qwertyNippon <orlandohenry100@gmail.com>
2023-05-29 22:01:37 -0400

    fixed some parts

--- parkingLotProject.py
+++ parkingLotProject.py
@@ -1,44 +1,36 @@
-tickets=[10]
-parkingSpaces=[10]
-currentTicket={False}
-currentTicketCount={}
+tickets=10
+parkingSpaces=10
+currentTicket={"paid":False, "amount":0}
+
 
 class Parking():
 
-    def __init__(self, ticket, parkingSpace):
+    def __init__(self, ticket, parkingSpace, currentTicketCount):
         self.ticket = ticket
         self.parkingSpace = parkingSpace
+        self.currTickCount = currentTicketCount
 
     def take_ticket(self):
-        for i in tickets:
-            tickets -= i
-            print(i)
-            print(tickets)
-            for i in parkingSpaces:
-                parkingSpaces -= i
-                print(i)
-                print(parkingSpaces)
-            
-
-# for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
-    # i = i - 1
-    # print i
+        print("congrats you have a ticket")
+        tickets -= 1
+        parkingSpaces -= 1
             
 # - This should decrease the amount of tickets available by 1
 # - This should decrease the amount of parkingSpaces available by 1
     def pay_parking(self):
-        payment = input(f"Welcome to sustainable pavement car parking."
-                        f"It is $1.00 for 15 mins or parking, select space to continue." 
-                        f"If you wish to end press 'q'. ")
+        payment = input(f"Welcome to sustainable pavement car parking.\nIt is $1.00 for 15 mins or parking, select space to continue.\nIf you wish to end press 'q'. ")
+        
         if payment == 'q':
             return
-        elif '':
+        elif payment == 1:
             print("Your ticket has been paid and you have 15 mins to leave or else...")
-            currentTicketCount +=1
-            currentTicket={True}
-            print(currentTicketCount)
-            print(currentTicket)
-
+            self.currentTicketCount += 1
+            self.currentTicket["paid"] = True
+            # referring to the key.
+            print(self.currentTicketCount)
+            print(self.currentTicket)
+        else:
+            print(f"sorry we do not serve dinosaur juice lovers here.")
             # update the "currentTicket" dictionary key "paid" to True
 
             
